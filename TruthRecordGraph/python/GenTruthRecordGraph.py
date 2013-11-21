cf846ec751f457f92f30c671ffaf7f9177edc603
bdjackson <bjack3@gmail.com>
2013-11-21 13:26:54 +0100

    printing dot file - eps won't display

--- TruthRecordGraph/python/GenTruthRecordGraph.py
+++ TruthRecordGraph/python/GenTruthRecordGraph.py
@@ -36,31 +36,38 @@ def printTruthRecord(event, max_depth):
 
 # ------------------------------------------------------------------------------
 def constructGraph(event, current_index, current_depth, max_depth):
-    this_graph_block = '\n'
+    label_block = ''
+    labels = {}
+    draw_labels = {}
+    # this_graph_block = '\n'
     for mc_index in xrange(event.mc_barcode.size()):
         # print 'checking mcindex: %s' % mc_index
         current_barcode = event.mc_barcode.at(mc_index)
         current_pdgid   = event.mc_pdgId.at(mc_index)
 
-        # print '  current barcode: %s' % current_barcode
-        # print '  current pdgid: %s' % current_pdgid
+        label_block += '  bc_%s [label="%s"];\n' % (current_barcode, current_pdgid)
 
-        this_graph_block += '  %s [label="%s"];\n' % (current_barcode, current_pdgid)
+    decay_block = ''
+    # # max_itr = min(event.mc_barcode.size(), max_depth)
+    max_itr = event.mc_barcode.size()
+    for mc_index in xrange(max_itr):
+        current_barcode = event.mc_barcode.at(mc_index)
+        child_indices = event.mc_child_index.at(mc_index)
+        parent_indices = event.mc_parent_index.at(mc_index)
+        # print 'child indices: %s' % child_indices
+        print 'num child indices: %s' % child_indices.size()
+        print 'num parent indices: %s' % parent_indices.size()
 
-        # print this_graph_block
+        for ci in child_indices:
+            print ci
+            child_barcode = event.mc_barcode.at(ci)
+            label_block += '  bc_%s -- bc_%s;\n' % (current_barcode, child_barcode)
 
-    # max_itr = min(event.mc_barcode.size(), max_depth)
-    # for mc_index in xrange(max_itr):
-    #     current_barcode = event.mc_barcode.at(mc_index)
-    #     child_indices = event.mc_child_index
-    #     # print 'child indices: %s' % child_indices
-    #     print 'num child indices: %s' % child_indices.size()
 
-    #     # for ci in child_indices:
-    #     #     print ci
-    #     #     child_barcode = event.mc_barcode.at(ci)
-    #     #     this_graph_block += '  %s -- %s;\n' % (current_barcode, child_barcode)
-
+    this_graph_block = '\n'
+    this_graph_block += label_block
+    this_graph_block += '\n'
+    this_graph_block += decay_block
 
     return this_graph_block
 
