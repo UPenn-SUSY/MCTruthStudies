c8dd8b87d7faae512193ed2ada80b7e57b55b94a
bdjackson <bjack3@gmail.com>
2013-11-25 16:10:45 +0100

    add 2d plots to plotting script

--- Plotting/HistMerger.py
+++ Plotting/HistMerger.py
@@ -10,17 +10,26 @@ import rootlogon
 
 # ==============================================================================
 class HistMerger(object):
-    def __init__(self, file_handles, hist_name, normalize = True):
+    def __init__(self, file_handles, hist_name, normalize = True, log = False):
         self.name = hist_name
         self.file_names = [fh.title              for fh in file_handles]
         self.hists = [fh.getHist(hist_name, normalize) for fh in file_handles]
         self.setStyle(file_handles, self.hists)
+        self.normalize = normalize
+        self.log = log
 
-        title = '%s;%s;%s' % ( self.hists[0].GetTitle()
-                             , self.hists[0].GetXaxis().GetTitle()
-                             , self.hists[0].GetYaxis().GetTitle()
-                             )
-        self.stack = ROOT.THStack('s_%s' % hist_name, title)
+        self.title = '%s;%s;%s' % ( self.hists[0].GetTitle()
+                                  , self.hists[0].GetXaxis().GetTitle()
+                                  , self.hists[0].GetYaxis().GetTitle()
+                                  )
+        if isinstance(self.hists[0], ROOT.TH2):
+            self.prep2DHistStack()
+        elif isinstance(self.hists[0], ROOT.TH1):
+            self.prep1DHistStack()
+
+    # ------------------------------------------------------------------------------
+    def prep1DHistStack(self):
+        self.stack = ROOT.THStack('s_%s' % self.name, self.title)
 
         leg_x1 = 0.80
         leg_x2 = 0.98
@@ -39,15 +48,64 @@ class HistMerger(object):
             self.legend.AddEntry(h, self.file_names[i], 'L')
             self.big_legend.AddEntry(h, self.file_names[i], 'L')
 
-        canvas_name = hist_name.replace('h__', 'c__')
-        if normalize:
-            canvas_name = canvas_name + "_norm"
+        canvas_name = self.name.replace('h__', 'c__')
+        if self.normalize:
+            canvas_name = canvas_name + "__norm"
+        if self.log:
+            canvas_name = canvas_name + "__log_y"
         self.canvas = ROOT.TCanvas(canvas_name)
+        if self.log:
+            self.canvas.SetLogy()
         self.stack.Draw('nostack')
         self.legend.Draw()
 
         self.leg_canvas = ROOT.TCanvas('%s__leg' % canvas_name)
         self.big_legend.Draw()
+
+    # ------------------------------------------------------------------------------
+    def prep2DHistStack(self):
+        canvas_name = self.name.replace('h__', 'c__')
+        if self.normalize:
+            canvas_name = canvas_name + "__norm"
+        if self.log:
+            canvas_name = canvas_name + "__log_z"
+        self.canvas = ROOT.TCanvas(canvas_name)
+
+        self.labels = []
+
+        if self.log:
+            self.canvas.SetLogz()
+
+        if len(self.hists) == 1:
+            print 'printing 2D as colz!'
+            self.hists[0].Draw('COLZ')
+
+            this_label = self.getLabel(0)
+            this_label.Draw()
+            self.labels.append(this_label)
+        else:
+            if len(self.hists) == 2:
+                self.canvas.Divide(1,2)
+            elif len(self.hists) < 5:
+                self.canvas.Divide(2,2)
+            elif len(self.hists) < 7:
+                self.canvas.Divide(2,3)
+            elif len(self.hists) < 10:
+                self.canvas.Divide(3,3)
+            else:
+                self.canvas.Divide(3,3)
+                print 'Uh Oh! HistMerger cannot handle histogram lists of > 9 entries'
+            
+            for i, h in enumerate(self.hists):
+                if i > 9: break
+                self.canvas.cd(i+1)
+                self.hists[i].Draw('COLZ')
+
+                this_label = self.getLabel(i)
+                this_label.Draw()
+                self.labels.append(this_label)
+
+        self.leg_canvas = None
 
     def setStyle(self, file_handles, hists):
         for fh,h in zip(file_handles, hists):
@@ -56,3 +114,11 @@ class HistMerger(object):
             h.SetLineStyle(fh.line)
             h.SetLineColor(fh.color)
             h.SetLineWidth(4)
+
+    def getLabel(self, index):
+        this_label = ROOT.TLatex(1,1, self.file_names[index])
+        this_label.SetNDC()
+        this_label.SetX(0.45)
+        this_label.SetY(0.80)
+        this_label.SetTextSize(0.05)
+        return this_label
--- Plotting/PlotTruthDists.py
+++ Plotting/PlotTruthDists.py
@@ -18,30 +18,34 @@ import FileHandle
 def main():
     config_file_name = sys.argv[1]
     files = FileHandle.readInputConfig(config_file_name)
+    print 'FILES: %s' % files
 
     out_file = ROOT.TFile.Open('truth_compare.canv.root', 'RECREATE')
     out_file.cd()
     list_of_hists = files[0].getListOfHists()
     num_hists = len(list_of_hists)
     for i, loh in enumerate(list_of_hists):
-        print 'hist (%d of %d): %s' % (i, num_hists, loh)
+        for is_log in [True, False]:
+            print 'hist (%d of %d): %s' % (i, num_hists, loh)
 
-        hm = HistMerger.HistMerger( files, loh, False )
-        hm.canvas.Write()
+            hm = HistMerger.HistMerger( files, loh, False , is_log)
+            hm.canvas.Write()
 
-        hm_norm = HistMerger.HistMerger( files, loh, True )
-        hm_norm.canvas.Write()
+            hm_norm = HistMerger.HistMerger( files, loh, True, is_log)
+            hm_norm.canvas.Write()
 
-        if 'channel' in loh:
-            hm.leg_canvas.Write('c__leg')
+            if 'channel' in loh:
+                if hm.leg_canvas is not None:
+                    hm.leg_canvas.Write('c__leg')
 
-        hm.canvas.Clear()
-        hm_norm.canvas.Clear()
-        hm.leg_canvas.Clear()
-        for h in hm.hists:
-            h.Delete()
-        for h in hm_norm.hists:
-            h.Delete()
+            hm.canvas.Clear()
+            hm_norm.canvas.Clear()
+            if hm.leg_canvas is not None:
+                hm.leg_canvas.Clear()
+            for h in hm.hists:
+                h.Delete()
+            for h in hm_norm.hists:
+                h.Delete()
     out_file.Close()
 
 # ==============================================================================
