7b90180804aeed587fa05bb1c13f6dd04c922f26
bdjackson <bjack3@gmail.com>
2014-04-24 19:27:37 +0200

    add config for official samples

---
+++ configs/stopstop_official.py
@@ -0,0 +1,80 @@
+- label: 'm(#tilde{t}) = 100 GeV'
+  file:  output_hists.202641.directBL_100.root
+  dir:   ""
+  color: 602 # kBlue+2
+  shape: 20 # kFullCircle
+  line:  1
+  xsec:  5.59757e-01
+
+- label: 'm(#tilde{t}) = 200 GeV'
+  file:  output_hists.202633.directBL_200.root
+  dir:   ""
+  color: 603
+  shape: 20 # kFullCircle
+  line:  1
+  xsec: 1.85245e-02
+
+- label: 'm(#tilde{t}) = 300 GeV'
+  file:  output_hists.202634.directBL_300.root
+  dir:   ""
+  color: 604
+  shape: 20 # kFullCircle
+  line:  1
+  xsec: 1.99608e-03
+
+- label: 'm(#tilde{t}) = 400 GeV'
+  file:  output_hists.202635.directBL_400.root
+  dir:   ""
+  color: 601
+  shape: 20 # kFullCircle
+  line:  1
+  xsec:  3.5683e-04
+
+- label: 'm(#tilde{t}) = 500 GeV'
+  file:  output_hists.202636.directBL_500.root
+  dir:   ""
+  color: 633 # kRed+1
+  shape: 20 # kFullCircle
+  line:  1
+  xsec:  8.55847e-05
+
+- label: 'm(#tilde{t}) = 600 GeV'
+  file:  output_hists.202637.directBL_600.root
+  dir:   ""
+  color: 634
+  shape: 20 # kFullCircle
+  line:  1
+  xsec:  2.48009e-05
+
+- label: 'm(#tilde{t}) = 700 GeV'
+  file:  output_hists.202638.directBL_700.root
+  dir:   ""
+  color: 635
+  shape: 20 # kFullCircle
+  line:  1
+  xsec:  8.1141e-06
+
+- label: 'm(#tilde{t}) = 800 GeV'
+  file:  output_hists.202639.directBL_800.root
+  dir:   ""
+  color: 636
+  shape: 20 # kFullCircle
+  line:  1
+  xsec:  2.89588e-06
+
+- label: 'm(#tilde{t}) = 900 GeV'
+  file:  output_hists.202640.directBL_900.root
+  dir:   ""
+  color: 632
+  shape: 20 # kFullCircle
+  line:  1
+  xsec:  1.09501e-06
+
+- label: 'm(#tilde{t}) = 1000 GeV'
+  file:  output_hists.202641.directBL_1000.root
+  dir:   ""
+  color: 419 # kGreen+3
+  shape: 20 # kFullCircle
+  line:  1
+  xsec:  4.35490e-07
+
